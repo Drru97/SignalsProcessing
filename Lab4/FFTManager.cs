c634be6d0d7e792a314d1bf0ca5afca36e33f09b
Drru97 <oleg.dystak@gmail.com>
2018-03-27 12:52:49 +0300

    Add Lab4.

---
+++ Lab4/FFTManager.cs
@@ -0,0 +1,83 @@
+﻿using Common.Models;
+using MathNet.Numerics.IntegralTransforms;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab4
+{
+	public class FFTManager : IFFTManager
+	{
+		private readonly Signal _signal = new Signal(4, 2, 1, 4, 6, 3, 5, 2);
+
+		public void GetFFT()
+		{
+			var result = CalculateFFT();
+
+			Console.WriteLine($"Signal = {_signal}");
+
+			Console.WriteLine($"FFT Real = {EnumerableToString(result.real)}");
+			Console.WriteLine($"FFT Imaginary = {EnumerableToString(result.imaginary)}");
+		}
+
+		private (double[] real, double[] imaginary) CalculateFFT()
+		{
+			const int roundDecimals = 2;
+			var real = GetRealPart();
+			var imaginary = GetImaginaryPart();
+
+			Fourier.Forward(real, imaginary);
+
+			Round(real, roundDecimals);
+			Round(imaginary, roundDecimals);
+
+			return (real, imaginary);
+		}
+
+		private double[] GetRealPart()
+		{
+			var bufferLength = _signal.Count % 2 == 0 ? _signal.Count + 2 : _signal.Count + 1;
+			var real = new double[bufferLength];
+
+			for (var i = 0; i < _signal.Count; i++)
+				real[i] = _signal[i];
+
+			return real;
+		}
+
+		private double[] GetImaginaryPart()
+		{
+			var bufferLength = _signal.Count % 2 == 0 ? _signal.Count + 2 : _signal.Count + 1;
+			var imaginary = new double[bufferLength];
+
+			return imaginary;
+		}
+
+		private static void Round(IList<double> list, int decimals)
+		{
+			for (var i = 0; i < list.Count; i++)
+			{
+				var rounded = Math.Round(list[i], decimals);
+				list[i] = rounded;
+			}
+		}
+
+		private static string EnumerableToString(IEnumerable<double> enumerable)
+		{
+			var sb = new StringBuilder();
+
+			sb.Append("{");
+			foreach (var item in enumerable)
+			{
+				sb.Append(item + ", ");
+			}
+
+			if (sb.Length > 2)
+				sb.Remove(sb.Length - 2, 2);
+
+			sb.Append("}");
+
+			return sb.ToString();
+		}
+	}
+}
---
+++ Lab4/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Lab4
+{
+	internal class Program
+	{
+		private static IServiceProvider _serviceProvider;
+
+		internal static void Main(string[] args)
+		{
+			ConfigureDependencies();
+
+			var manager = _serviceProvider.GetService<IFFTManager>();
+			manager.GetFFT();
+
+			Console.ReadKey();
+		}
+
+		private static void ConfigureDependencies()
+		{
+			var services = new ServiceCollection();
+
+			services
+				.AddTransient<IFFTManager, FFTManager>();
+
+			_serviceProvider = services.BuildServiceProvider();
+		}
+	}
+}
